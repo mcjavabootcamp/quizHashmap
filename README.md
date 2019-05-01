6b2a9bb4a6a0fe6aa8c353d6552260bda179de15
GBTC440001UR <GBTC440001UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-05-01 10:34:33 -0400

    intial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+
+# This program will not run. Please find the errors - there are 7 in total!
+
+
+package com.company;
+
+public class Main {
+
+	public static void main(String [] args) {
+	
+		// Initialize the hashmap
+		HashMap (int, String) myMap = new HashMap(int, String)<>;
+		
+		// Add keys and values
+		map.get(1, "oklahoma");
+		map.get(2, "maryland");
+		map.get(3, "virginia");
+		
+		// Get the value corresponding to the key 3
+		System.out.println("The value is " + map.put(3));
+		
+		// Check to see if "indiana" is in my HashMap
+		String myVal = "indiana";
+		for (String value : myVal.values()) {
+            if (myVal == value){
+               break; 
+            }
+        }
+        System.out.println(myVal + " is in the hashmap.");
+
+	
+	}
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+/*
+ * Q: What is the role of a constructor in the class?
+ *
+ * A: A constructor initializes the class
+ */
+package com.company;
+
+// import statements go after package declaration, and before class declaration
+import java.util.HashMap;
+public class Main {
+
+    public static void main(String [] args) {
+
+        // Initialize the hashmap
+        HashMap <Integer, String> myMap = new HashMap<Integer, String>();
+
+        // Add keys and values
+        myMap.put(1, "oklahoma");
+        myMap.put(2, "maryland");
+        myMap.put(3, "virginia");
+
+        // Get the value corresponding to the key 3
+        System.out.println("The value is " + myMap.get(3));
+
+        // Check to see if "indiana" is in my HashMap
+        String myVal = "virginia";
+        for (String value : myMap.values()) {
+            if (myVal==value){
+                System.out.println(myVal + " is in the hashmap.");
+                break;
+
+            }
+        }
+
+    }
+
+}
